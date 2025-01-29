082b9745626a75cd1ddabe0d38f23080426eeae5
Isal0192 <faisalfajar1305@gmail.com>
2025-01-29 08:57:45 +0700

    update navigasi sesuai figma

--- src/component/fragment/Navigasi.jsx
+++ src/component/fragment/Navigasi.jsx
@@ -4,25 +4,29 @@ const Navigasi = () => {
     <nav className="bg-gray-800 p-4">
         <div className="container mx-auto flex items-center justify-between">
 
-          <div className="text-xl font-bold text-white">NAMA Website</div>
-
+          <div className="text-xl font-bold text-white">
+            <h1>NAMA Website</h1>
+            <hr />
+          </div>
+          <hr className="line"/>
+          
           <ul className="hidden md:flex space-x-6 text-white">
             <li className="relative group">
-              <button className="hover:text-green-400">desain halaman ▼</button>
+              <button className="btn-aja hover:text-green-400">desain halaman <i className="rotate">▼</i></button>
               <ul className="absolute left-0 top-full hidden w-40 bg-gray-700 group-hover:block">
                 <li><a href="#" className="block px-4 py-2 hover:bg-gray-600">Halaman 1</a></li>
                 <li><a href="#" className="block px-4 py-2 hover:bg-gray-600">Halaman 2</a></li>
               </ul>
             </li>
             <li className="relative group">
-              <button className="hover:text-green-400">desain element ▼</button>
+              <button className="btn-aja hover:text-green-400">desain element <i className="rotate">▼</i></button>
               <ul className="absolute left-0 top-full hidden w-40 bg-gray-700 group-hover:block">
                 <li><a href="#" className="block px-4 py-2 hover:bg-gray-600">Element 1</a></li>
                 <li><a href="#" className="block px-4 py-2 hover:bg-gray-600">Element 2</a></li>
               </ul>
             </li>
             <li className="relative group">
-              <button className="hover:text-green-400">tips and trick ▼</button>
+              <button className="btn-aja hover:text-green-400 ">tips and trick <i className="rotate">▼</i></button>
               <ul className="absolute left-0 top-full hidden w-40 bg-gray-700 group-hover:block">
                 <li><a href="#" className="block px-4 py-2 hover:bg-gray-600">Tip 1</a></li>
                 <li><a href="#" className="block px-4 py-2 hover:bg-gray-600">Tip 2</a></li>
@@ -31,7 +35,8 @@ const Navigasi = () => {
           </ul>
 
           <div>
-            <a href="#" className="bg-red-500 hover:bg-red-600 px-4 py-2 rounded text-white">SIGN IN</a>
+            <a href="#" className="btn-collor px-4 py-2 rounded-full text-black font-bold text-center">+ create</a>
+            <a href="#" className="px-4 py-2 color-black-00 font-bold text-center">SIGN IN</a>
           </div>
         </div>
     </nav>
